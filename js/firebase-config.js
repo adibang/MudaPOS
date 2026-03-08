b712f9cd07e587a5d4263fa6974a54a841a8ecd9
adibang <adiblinggo@gmail.com>
2026-03-08 12:55:02 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,12 +5,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChang
 // Dexie sudah tersedia secara global dari tag script di HTML, tidak perlu import
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDvty30S6y_qc1eMs3kVctcIpLt6Suw4dA",
-    authDomain: "mudapos-e8a4a.firebaseapp.com",
-    projectId: "mudapos-e8a4a",
-    storageBucket: "mudapos-e8a4a.firebasestorage.app",
-    messagingSenderId: "864215339664",
-    appId: "1:864215339664:web:1a97898b85ca3c1cb3be15"
+  apiKey: "AIzaSyDVty30S6y_qc1eMs3kVctcIpLt6Suw4dA",
+  authDomain: "mudapos-e8a4a.firebaseapp.com",
+  projectId: "mudapos-e8a4a",
+  storageBucket: "mudapos-e8a4a.firebasestorage.app",
+  messagingSenderId: "864215339664",
+  appId: "1:864215339664:web:1a97898b85ca3c1cb3be15"
 };
 
 const app = initializeApp(firebaseConfig);
