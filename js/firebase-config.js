ca592ef96b40597a0f58b8eba2046c05e5fd5fea
adibang <adiblinggo@gmail.com>
2026-03-08 22:54:26 +0700

    Mengganti dixie dengan localforage

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -16,20 +16,4 @@ const dbCloud = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-// Inisialisasi Dexie dengan semua tabel yang diperlukan
-const dbLokal = new Dexie("MudaPosDB");
-dbLokal.version(1).stores({
-    produk: '++id, kode, nama, barcode, kategoriId, hargaDasar, hargaJual, berat, unitId, diskon, gudangId, stokAwal, stokMinimal, produkTimbangan, synced, idCloud',
-    konversiUnit: '++id, produkId, produkIdCloud, unitId, nilai, barcode, aktif, hargaDasar, hargaJual, poinPelanggan, komisiSales, synced, idCloud',
-    qtyBreak: '++id, produkId, produkIdCloud, level, minQty, harga, synced, idCloud',
-    kategori: '++id, nama, synced, idCloud',
-    unit: '++id, nama, synced, idCloud',
-    gudang: '++id, kode, nama, lokasi, kapasitas, synced, idCloud',
-    transaksi: '++id, noFaktur, tanggal, total, statusSync, synced, idCloud',
-    detailTransaksi: '++id, transaksiId, produkId, nama, harga, qty, subtotal, synced, idCloud'
-});
-
-// Ekspos dbLokal ke global agar bisa diakses di script biasa
-window.dbLokal = dbLokal;
-
-export { app, dbCloud, dbLokal, auth, provider, signInWithPopup, signOut, onAuthStateChanged };
+export { app, dbCloud, auth, provider, signInWithPopup, signOut, onAuthStateChanged };
